d02097e2e64f9e78100ab89477d0fce0f7bb6a22
AsirMahmud <131938070+AsirMahmud@users.noreply.github.com>
2024-08-08 14:59:22 +0600

    Update OurProfile.vue

--- components/OurProfile.vue
+++ components/OurProfile.vue
@@ -18,7 +18,7 @@ const pages = ref([
     <div class="bg-primaryb w-full flex justify-center items-center py-16">
         <div class="max-w-[800px] mx-auto  items-center p-16 flex flex-col gap-8">
             <div class="flex flex-col gap-4 text-center">
-                <h1 class="text-2xl font-bold">Our Profile</h1>
+                <h1 class="lg:text-4xl text-2xl  font-bold">Our Profile</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque rem autem error laborum quisquam ad
                     esse saepe tempore cum molestias minus modi, mollitia fugit assumenda aut similique fuga sit illo.
                 </p>
